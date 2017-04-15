dcbc69f36bca58fce0d21fd06c263e85ec96bbc4
Yu Wan <wanyuac@126.com>
2017-04-15 12:42:27 +1000

    updated unlinkFiles.sh

--- bash/unlinkFiles.sh
+++ bash/unlinkFiles.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Iterates the unlink command on multiple soft links.
 # Example:
-#   unlinkFiles.sh "*.fasta"
-#	unlinkFiles.sh "reads/*.fastq.gz"
-# Author: Yu Wan (14 April 2017) published at https://github.com/wanyuac/BINF_toolkit
+#	unlinkFiles.sh reads/*.fastq.gz
+#	unlinkFiles.sh $(cat link_list.txt)
+# Author: Yu Wan (14-15 April 2017) published at https://github.com/wanyuac/BINF_toolkit
 # License: Apache-2.0
 
-wildcard=$1
-links=(`ls -1 ${wildcard}`)  # read a list of files into an array
+links=( $@ )
 for i in ${links[@]}; do
     unlink $i
 done
