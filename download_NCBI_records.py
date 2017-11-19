9834c2f79d548e06a203c64a2e88a26e2c610d83
Yu Wan <wanyuac@126.com>
2017-11-19 18:30:31 +1100

    Make it compatible to Python 3

--- download_NCBI_records.py
+++ download_NCBI_records.py
@@ -21,15 +21,17 @@ References:
 	
 Author: Yu Wan (wanyuac@gmail.com, https://github.com/wanyuac)
 Development history
-	27 June 2015 - 14 July 2015, 4 November 2015, 6/2/2016, 23/7/2016
+	27 June 2015 - 14 July 2015, 4 November 2015, 6/2/2016, 23/7/2016, 19/11/2017
 	Previous name: download_gbk.py
-Python version: 2.7.5
+Python version 2 and 3 compatible
 Licence: GNU GPL 2.1
 """
 
+from __future__ import print_function
 from Bio import Entrez
 from argparse import ArgumentParser
 import time, os
+
 
 def parse_arguments():
 	parser = ArgumentParser(description="Read options and arguments")
@@ -41,6 +43,7 @@ def parse_arguments():
 	parser.add_argument("--outdir", "-o", dest = "outdir", type = str, default = ".", required = False, help = "Destination directory, no backslash at the end")
 	parser.add_argument("--skip", "-sk", dest = "skip", action = "store_true", required = False, help = "Set to skip downloaded files")
 	return parser.parse_args()
+
 		
 def main():
 	args = parse_arguments()
@@ -67,7 +70,7 @@ def main():
 			new_file = os.path.join(args.outdir, entry + extension)
 			
 		if os.path.exists(new_file) and args.skip:
-			print new_file + " already exists, skipped."
+			print(new_file + " already exists, skipped.")
 			continue  # go to the next entry
 		try:
 			if args.format == "fasta":
@@ -75,16 +78,17 @@ def main():
 			else:
 				handle = Entrez.efetch(db = "nucleotide", id = entry, rettype = "gbwithparts", retmode = "text")  # not "gb" for rettype, as it only includes contig locations if the entry is built from contigs
 			with open(new_file, "w") as output_file:
-				print "Downloading " + entry + " to " + new_file
+				print("Downloading " + entry + " to " + new_file)
 				output_file.write(handle.read())  # read and write this entry into a new file named after its accession number
 			n += 1
 			handle.close()
 		except:
-			print "The record "+ entry + " is not found."
+			print("The record "+ entry + " is not found.")
 			continue
-		time.sleep(1) # should not send too many requests per second to NCBI
+		time.sleep(1) # Pause for one second to obviate submitting too many concurrent requests to NCBI
 	
-	print "Done. Altogether %d files were downloaded and stored in %s successfully." % (n, args.outdir)
+	print("Done. Altogether %d files were downloaded and stored in %s successfully." % (n, args.outdir))
+	
 
 if __name__ == "__main__":
 	main()
