40c4921140663c1847e8fffd427990e7920cf611
wanyuac <wanyu@microbialsystems.cn>
2021-03-31 13:57:37 +0100

    Update code

--- shell/download_ena_reads.sh
+++ shell/download_ena_reads.sh
@@ -16,31 +16,49 @@ display_usage() {
     "
 }
 
+# Construct the downloader function download_and_check ####################
 display_cmd() {
+    # A subordinate function of download_reads.
     echo "
 # Download $2 ==> $1:
 wget --timeout=20 --waitretry=2 --tries=3 --retry-connrefused --no-verbose --no-glob --output-document $1 $2
-sleep 5
+sleep 6
     "
 }
 
 download_reads() {
     # $1: output filename; $2: FTP address of the remote read file
+    # A subordinate function of download_and_check.
     if [ ! -f "$1" ]; then
         display_cmd $1 $2
         wget --timeout=20 --waitretry=2 --tries=3 --retry-connrefused --no-verbose --no-glob --output-document $1 $2
-        sleep 5
+        sleep 6  # Waiting for 2-4 seconds may not be able to avoid the problem of "Gateway timeout"
     else
         echo "# Skip downloading for $1 as it exists."
     fi
 }
 
+download_and_check() {
+    # $1: output filename; $2: FTP address of the remote read file
+    # retries=`seq 1 1 3`
+    download_reads $1 $2  # Rety three times if the problem of "Gateway timeout" keeps occurring.
+    #for i in ${retries[@]}; then
+    for i in 1 2 3; do
+        if [ -s "$1" ]; then
+            break
+        else
+            rm -f $1
+            download_reads $1 $2  # Download the file again if the existing one has a size of zero (due to the error of "Gateway timeout")
+        fi
+    done
+}
+
+# Main #########################
 if [ -z $1 ]; then
     display_usage
     exit
 fi
 
-# Main #########################
 # Set up output directory
 if [ -z $2 ]; then
     outdir=$PWD
@@ -55,26 +73,13 @@ else
     echo "# Read directory $outdir exists."
 fi
 
-while read line; do
+while read line; do  # Read through the input TSV file line-by-line
     IFS=$'\t' read -r -a fields <<< "$line"
-
-    # Setting up filenames
     isolate=${fields[0]}
     url1=${fields[1]}
     url2=${fields[2]}
     readfile1="${outdir}/${isolate}_1.fastq.gz"
     readfile2="${outdir}/${isolate}_2.fastq.gz"
-
-    # Start the downloading process
-    download_reads $readfile1 $url1
-    if [ ! -s "$readfile1" ]; then
-        rm -f $readfile1
-        download_reads $readfile1 $url1  # Download the file again if the existing one has a size of zero (due to the error of "Gateway timeout")
-    fi
-
-    download_reads $readfile2 $url2
-    if [ ! -s "$readfile2" ]; then
-        rm -f $readfile2
-        download_reads $readfile2 $url2  # Download the file again if the existing one has a size of zero (due to the error of "Gateway timeout")
-    fi
-done < "$1"  # expect a file name as an input
+    download_and_check $readfile1 $url1
+    download_and_check $readfile2 $url2
+done < "$1"  # Expect a file name as an input
