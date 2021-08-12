ad8f121b0178744afd2af9b9677cb8716cb4fac4
Yu Wan <wanyuac@126.com>
2021-08-12 23:26:09 +0100

    Update seqlen.py

--- seqlen.py
+++ seqlen.py
@@ -4,10 +4,13 @@
 Calculate sequence lengths in a multiFASTA file.
 This script was inspired by Lesley Sitter's code published on biostars (www.biostars.org/p/148815/).
 
-Command: python seqlen.py [input FASTA file] > [output TSV file]
+Command: python seqlen.py [input FASTA file] [any character] > [output TSV file]
+
+Any character: a flag to keep sequence annotation in the output.
 
 Copyright (C) 2021 Yu Wan <wanyuac@126.com>
 Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
+Release: 2021; latest update: 12 Aug 2021
 """
 
 import sys
@@ -17,13 +20,15 @@ with open(sys.argv[1], "r") as f:
 print("\t".join(["Name", "Length"]))  # The header line
 seq = ""
 allow_write = False  # A flag indicating that the first sequence has been completely loaded.
+ignore_annot = len(sys.argv) < 3
 
 for line in input_fasta:
     if line.startswith(">"):  # A new sequence is encountered
         if allow_write:
             print("\t".join([seqid, str(len(seq))]))
             seq = ""
-        seqid = line.split(" ")[0]  # Get the sequence ID and ignore the sequence annotation
+        if ignore_annot:
+            seqid = line.split(" ")[0]  # Get the sequence ID and ignore the sequence annotation
         seqid = seqid[1 : ]  # Drop the ">" character
         allow_write = True
     else:
