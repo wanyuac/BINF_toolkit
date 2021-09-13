00f1b771728c2795bc49edfbf625fc274a96afe8
wanyuac <wanyuac@126.com>
2021-09-13 22:22:55 +0100

    improvement

--- extractSeqFromMultiFASTA.py
+++ extractSeqFromMultiFASTA.py
@@ -2,7 +2,8 @@
 
 """
 This script extracts sequences from a multi-FASTA file using sequence IDs, which are defined in sequence headers. It
-basically filters contigs from a multi-FASTA file based on sequence IDs.
+basically filters contigs from a multi-FASTA file based on sequence IDs. The script ignores sequence annotation that
+is separated from the sequence ID by a white space.
 
 Input: a multi-FASTA file from stdin. It can be a gene-feature file (.ffn) downloaded from the NCBI nucleotide database
 or an assembly file comprised of several contig sequences.
@@ -15,11 +16,14 @@ Usage:
     Or,
     targets=$(cat seqIDs.txt)  # seqIDs.txt contains a single comma-delimited line.
     cat input.fna | python extractSeqFromMultiFASTA.py $targets > output.fna
+    For SPAdes assemblies:
+    targets=$(cat seqIDs.txt)
+    cat input__scaffolds.fna | sed 's/_length_/ /g' | python extractSeqFromMultiFASTA.py $targets > input__scaffolds_subset.fna
 
 Author: Yu Wan (wanyuac@126.com, https://github.com/wanyuac)
 Python version 2 and 3 compatible
 License: GNU GPL 2.1
-First edition: 5 July 2016; the latest edition: 25 May 2020
+First edition: 5 July 2016; the latest edition: 13 Sep 2021
 Previous name: extract_fasta_loci.py
 """
 
